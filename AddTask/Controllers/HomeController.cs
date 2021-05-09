866a686bd9680ebbf4411f20bbed5a6e07d8aec1
Denis <denis_aleksandrov_12@mail.ru>
2021-05-09 22:50:58 +0300

    Add project files.

---
+++ AddTask/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using AddTask.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AddTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            string[] courses = { "JavaScript", "C#", "Java", "Python", "Основы" };
+            SelectList list = new SelectList(courses, courses[1]);
+            ViewBag.SList = list;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegistrationBindingModel registration, string listItem)
+        {
+            if (registration.Date.DayOfWeek == DayOfWeek.Sunday | registration.Date.DayOfWeek == DayOfWeek.Saturday)
+            {
+                ModelState.AddModelError(nameof(registration.Date), "Консультации не осуществляются в выходные дни!");
+            }
+            if (registration.Date.DayOfYear < DateTime.Now.DayOfYear)
+            {
+                ModelState.AddModelError(nameof(registration.Date), "Дата консультации должна быть в будующем времени!");
+            }
+
+            if (listItem == "Основы" & registration.Date.DayOfWeek == DayOfWeek.Monday)
+            {
+                ModelState.AddModelError(nameof(registration.Date), "По курсу \"Основы\" занятия не проводятся в понедельник!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                ViewBag.FName = registration.FName;
+                ViewBag.Date = registration.Date.ToShortDateString();
+                ViewBag.Course = listItem;
+                return View("Success");
+            }
+            else
+            {
+                return View(registration);
+            }
+        }
+    }
+}
---
+++ AddTask/Models/RegistrationBindingModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddTask.Models
+{
+    public class RegistrationBindingModel
+    {
+        [Required]
+        [StringLength(15)]
+        [Display(Name = "Имя")]
+        public  string FName { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "Фамилия")]
+        public  string LName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public  string Email { get; set; }
+
+        [UIHint("DateTime-local")]
+        [Required]
+        [Display(Name = "Желаемая дата консультации")]
+        public  DateTime Date { get; set; }
+    }
+}
